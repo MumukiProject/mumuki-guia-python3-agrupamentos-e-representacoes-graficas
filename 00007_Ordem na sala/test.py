c6a41567f95316abd30c3ab91e27ef4d6e72adbd
franco@mumuki.org <franco@mumuki.org>
2022-10-17 23:18:44 +0000

    Mumuki Export on 2022-10-17 23:18:44 +0000

--- 00007_Ordem na sala/test.py
+++ 00007_Ordem na sala/test.py
@@ -1,9 +1,9 @@
 class Test(unittest.TestCase):
 
-  def test_pantallas_por_sector_es_un_series(self):
-    self.assertEquals(type(provincias_con_cines_grandes), pd.Series)
+  def test_estados_com_cinemas_grandes_é_um_series(self):
+    self.assertEquals(type(estados_com_cinemas_grandes), pd.Series)
     
   def test_genera_el_series_correcto(self):
-    self.assertEquals(provincias_con_cines_grandes.to_dict(), {
+    self.assertEquals(estados_com_cinemas_grandes.to_dict(), {
         'Santa Fe': 584.0, 'Buenos Aires': 1036.0, 'San Juan': 1071.0
     })
