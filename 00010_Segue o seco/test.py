dd7cd0cd71db98c42b6c6ddc31cd36154f0b7982
franco@mumuki.org <franco@mumuki.org>
2022-11-11 16:54:41 +0000

    Mumuki Export on 2022-11-11 16:54:41 +0000

--- 00010_Segue o seco/test.py
+++ 00010_Segue o seco/test.py
@@ -5,10 +5,10 @@ class Test(unittest.TestCase):
     
   def test_gera_o_DataFrame_correto(self):
     indexadas = assentos_per_localidade.set_index("city")
-    self.assertTrue(len(assentos_per_localidade) == 9, "debe tener el tamaño correcto")
+    self.assertTrue(len(assentos_per_localidade) == 11, "debe tener el tamaño correcto")
     self.assertTrue(indexadas.loc["Bragado", "seats"] == 750, "Tiene la cantidad correcta de Bragado")
     self.assertTrue(indexadas.loc["Salta", "seats"] == 354, "Tiene la cantidad correcta de Salta")
     self.assertTrue(indexadas.loc["Pilar", "seats"] == 1407, "Tiene la cantidad correcta de Pilar")
     
   def test_genera_as_colunas_corretas(self):  
-    self.assertEquals(list(assentos_per_localidade.columns), ['city', 'seats'])
+    self.assertEquals(set(assentos_per_localidade.columns), {'province', 'department', 'city', 'seats'})
