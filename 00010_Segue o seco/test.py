e4179fbbf8f02f118e2f4b42ca95cb7781f45162
franco@mumuki.org <franco@mumuki.org>
2022-11-11 17:00:33 +0000

    Mumuki Export on 2022-11-11 17:00:33 +0000

--- 00010_Segue o seco/test.py
+++ 00010_Segue o seco/test.py
@@ -5,10 +5,14 @@ class Test(unittest.TestCase):
     
   def test_gera_o_DataFrame_correto(self):
     indexadas = assentos_per_localidade.set_index("city")
-    self.assertTrue(len(assentos_per_localidade) == 11, "debe tener el tamaño correcto")
-    self.assertTrue(indexadas.loc["Bragado", "seats"] == 750, "Tiene la cantidad correcta de Bragado")
-    self.assertTrue(indexadas.loc["Salta", "seats"] == 354, "Tiene la cantidad correcta de Salta")
-    self.assertTrue(indexadas.loc["Pilar", "seats"] == 1407, "Tiene la cantidad correcta de Pilar")
+    self.assertTrue(len(assentos_per_localidade) == 11, "deve ser do tamanho correto")
+    self.assertTrue(indexadas.loc["Bragado", "seats"] == 750, "Bragado deve ter a quantidade correta")
+    self.assertTrue(indexadas.loc["Salta", "seats"] == 354, "Salta deve ter a quantidade correta")
+    self.assertTrue(indexadas.loc["Pilar", "seats"] == 1407, "Pilar deve ter a quantidade correta")
+
+  def test_tem_duas_cidades_colon(self):
+    assert len(butacas_por_localidad[butacas_por_localidad["city"]== "Colón"]) == 2, "deve ter duas cidades Colón"
+  
     
   def test_genera_as_colunas_corretas(self):  
     self.assertEquals(set(assentos_per_localidade.columns), {'province', 'department', 'city', 'seats'})
