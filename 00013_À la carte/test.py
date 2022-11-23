f559e87f0bf8a9fc4c01d1bb6995316498fd82a7
franco@mumuki.org <franco@mumuki.org>
2022-11-23 15:13:44 +0000

    Mumuki Export on 2022-11-23 15:13:44 +0000

--- 00013_À la carte/test.py
+++ 00013_À la carte/test.py
@@ -1,4 +1,18 @@
 class Test(unittest.TestCase):
 
-  def test_description_example(self):
-    self.assertTrue(True)
+  def test_proporcao_cinemas_comerciais_estatais_é_um_DataFrame(self):
+    self.assertEquals(type(proporcao_cinemas_comerciais_estatais), pd.DataFrame)
+    
+  def test_gera_o_DataFrame_correto(self):
+    indexados = proporcao_cinemas_comerciais_estatais.set_index("province")
+    self.assertTrue(len(proporcao_cinemas_comerciais_estatais) == 5, "deve ser do tamanho correto")    
+    self.assertTrue(indexados.loc["San Juan", "sector"] == 1, "deve ter o valor correto para San Juan")
+    self.assertTrue(indexados.loc["Buenos Aires", "sector"] == 0.8, "deve ter o valor correto para Buenos Aires")
+    self.assertTrue(indexados.loc["Santa Fe", "sector"] == 1, "deve ter o valor correto para Santa Fe")
+    
+  def test_gera_as_colunas_corretas(self):  
+    self.assertEquals(list(proporcao_cinemas_comerciais_estatais.columns), ['province', 'sector'])   
+
+  def test_é_ordenado_do_menor_para_o_maior(self):  
+    self.assertTrue(proporcao_cinemas_comerciais_estatais["sector"].iloc[0] == 0, "o primeiro elemento deve ser o mínimo")   
+  
