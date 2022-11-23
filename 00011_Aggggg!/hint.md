d99c257f9447dae6d58840f1c7daea74465523cd
franco@mumuki.org <franco@mumuki.org>
2022-11-23 15:29:20 +0000

    Mumuki Export on 2022-11-23 15:29:20 +0000

---
+++ 00011_Aggggg!/hint.md
@@ -0,0 +1,1 @@
+Assim como as agregações anteriores, `agg` retorna outro `DataFrame`, então podemos classificar seu resultado usando `sort_values`.
--- 00011_Aggggg!/test.py
+++ 00011_Aggggg!/test.py
@@ -1,18 +1,22 @@
 class Test(unittest.TestCase):
 
-  def test_proporcao_cinemas_comerciais_estatais_é_um_DataFrame(self):
-    self.assertEquals(type(proporcao_cinemas_comerciais_estatais), pd.DataFrame)
+  def test_medianas_por_provincia_e_um_DataFrame(self):
+    self.assertEquals(type(medianas_por_provincia), pd.DataFrame)
     
-  def test_gera_o_DataFrame_correto(self):
-    indexados = proporcao_cinemas_comerciais_estatais.set_index("province")
-    self.assertTrue(len(proporcao_cinemas_comerciais_estatais) == 5, "deve ser do tamanho correto")    
-    self.assertTrue(indexados.loc["San Juan", "sector"] == 1, "deve ter o valor correto para San Juan")
-    self.assertTrue(indexados.loc["Buenos Aires", "sector"] == 0.8, "deve ter o valor correto para Buenos Aires")
-    self.assertTrue(indexados.loc["Santa Fe", "sector"] == 1, "deve ter o valor correto para Santa Fe")
+  def test_gera_o_DataFrame_corteto(self):
+    indexados = medianas_por_provincia.set_index("province")
+    self.assertTrue(len(medianas_por_provincia) == 5, "deve ter o tamanho correto")    
+    self.assertTrue(indexados.loc["San Juan", "screens"] == 6, "deve ter o valor correto de telas para San Juan")
+    self.assertTrue(indexados.loc["Buenos Aires", "seats"] == 750, "deve ter o valor correto de assentots para Buenos Aires")
+    self.assertTrue(indexados.loc["Santa Fe", "seats"] == 584, "deve ter o valor correto de assentots para Santa Fe")
     
   def test_gera_as_colunas_corretas(self):  
-    self.assertEquals(list(proporcao_cinemas_comerciais_estatais.columns), ['province', 'sector'])   
+    self.assertEquals(set(medianas_por_provincia.columns), {'province', 'screens', 'seats'})   
+    
 
-  def test_é_ordenado_do_menor_para_o_maior(self):  
-    self.assertTrue(proporcao_cinemas_comerciais_estatais["sector"].iloc[0] == 0, "o primeiro elemento deve ser o mínimo")   
+  def test_esta_ordenado_de_a_a_z(self):  
+    self.assertTrue(medianas_por_provincia["province"].iloc[0] == "Buenos Aires", "o primer elemento deve ser Buenos Aires") 
+    self.assertTrue(medianas_por_provincia["province"].iloc[1] == "Ciudad Autónoma de Buenos Aires", "Ciudad Autónoma de Buenos Aires deve estar depois da Buenos Aires")
+    self.assertTrue(medianas_por_provincia["province"].iloc[2] == "Salta", "Salta deve estar depois da Ciudad Autónoma de Buenos Aires")
+    self.assertTrue(medianas_por_provincia["province"].iloc[4] == "Santa Fe", "Santa Fe deve estar depois de Salta")
   
